507f8138e92ad4c965bdad1b270862bd4cd2ced0
Serafima <sima14@i.ua>
2018-11-20 14:55:21 +0200

    babel was installed and tested

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'js/main.js',
+    },
+
+    // ...previous Webpack config...
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            // ...additional rules...
+        ],
+    },
+};
